d943c0c006cedd89ca2ccf05f581981efc11796b
Muskanshekhawat1808 <83718152+Muskanshekhawat1808@users.noreply.github.com>
2021-05-12 14:01:56 +0530

    Create Two_variable's_value.c

---
+++ Two_variable's_value.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+// Function to find the square-root of N
+double findSQRT(double N) { return sqrt(N); }
+
+int main()
+{
+	// Given number
+	int N = 12;
+
+	// Function call
+	printf("%f ", findSQRT(N));
+	return 0;
+}
+© 2021 GitHub, Inc.
